7488083678a582c434accf935e53cbd86cbd1579
cameronapak <cameronandrewpak@gmail.com>
2025-04-04 11:47:28 -0500

    Update seed data to include HTML formatting for notes, enhancing readability and presentation.

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,13 @@
 import { db, Notes } from 'astro:db';
 
 const CAMS_TEST_ID = "user_2sPEHVtl8DNuI1tn3HKiepIEnyz";
-const DEREK_NOTE = `Thank you so much for trying out this notes app designed for Bible study. It’s been a project I’ve been working on for a good amount of time. 
+const DEREK_NOTE = `<p>Thank you so much for trying out this notes app designed for Bible study. It's been a project I've been working on for a good amount of time.</p>
 
-The goal of Harvous is to be the digital tool and space for you and others to use for your Bible study. 
+<p>The goal of Harvous is to be the digital tool and space for you and others to use for your Bible study.</p>
 
-How it began...  I was frustrated with there not being a good notes app just for my Bible study. I used Apple Notes and made a ton of highlights in the YouVersion Bible app. I needed a better app, so I sought out to design one. And with the help of my friend, Cam, we built this Bible study notes app.
+<p>How it began...  I was frustrated with there not being a good notes app just for my Bible study. I used Apple Notes and made a ton of highlights in the YouVersion Bible app. I needed a better app, so I sought out to design one. And with the help of my friend, Cam, we built this Bible study notes app.</p>
 
-Please if you come across anything that isn’t working as it prolly should or if you have any feedback for us send it here.
-`
+<p>Please if you come across anything that isn't working as it prolly should or if you have any feedback for us send it here.</p>`
 
 // https://astro.build/db/seed
 export default async function seed() {
