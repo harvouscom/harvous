dcb06914d1cd4180adb3d53b289b0a30bab62b71
cameronapak <cameronandrewpak@gmail.com>
2025-05-27 17:04:47 -0500

    added commented code for when threads is added

--- db/config.ts
+++ db/config.ts
@@ -13,6 +13,28 @@ const Notes = defineTable({
   }
 })
 
+// const Threads = defineTable({
+//   columns: {
+//     id: column.number({ primaryKey: true }),
+//     title: column.text(),
+//     createdAt: column.date(),
+//     updatedAt: column.date({ optional: true }),
+//     userId: column.text(), // Clerk user id
+//     isPublic: column.boolean({ default: false }),
+//     color: column.text({ optional: true }), // Store the color name or value
+//     isPinned: column.boolean({ default: false }), // Whether the thread is pinned
+//   }
+// })
+
+// const NoteThreads = defineTable({
+//   columns: {
+//     id: column.number({ primaryKey: true }),
+//     noteId: column.number(),
+//     threadId: column.number(),
+//     createdAt: column.date(),
+//   }
+// })
+
 // https://astro.build/db/config
 export default defineDb({
   tables: {
